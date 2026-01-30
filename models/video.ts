ece2128c06526af9c9e11da70979aeb30c120ceb
Sohaib Arshid <sohaibali1277@gmail.com>
2026-01-30 16:11:53 +0500

    Globle veriable declare and video schema

--- models/video.ts
+++ models/video.ts
@@ -2,19 +2,45 @@
 import mongoose, { Schema, model, models } from "mongoose";
 
 export const Video_dimention = {
-    height : 1920,
-    width : 1080
+    height: 1920,
+    width: 1080
 } as const;
 
 // Typescript interface
 export interface IVideo {
-    _id? : mongoose.Types.ObjectId;
-    title : string;
-    discription : string;
-    videoUrl : string;
-    thumbnailUrl : string;
-    control? : boolean;
-    transformation? : boolean;
-} git add .
-git commit -m "implement User authentication model with hashed passwords"
-git push origin main
+    _id?: mongoose.Types.ObjectId;
+    title: string;
+    discription: string;
+    videoUrl: string;
+    thumbnailUrl: string;
+    control?: boolean;
+    transformation?: {
+        height: number;
+        width: number;
+        quality: number;
+    };
+}
+
+// Video Schema
+const videoschema = new Schema<IVideo>(
+    {
+        title: { type: String, required: true },
+        discription: { type: String, required: true },
+        videoUrl: { type: String, required: true },
+        thumbnailUrl: { type: String, required: true },
+        control: { type: Boolean, required: true },
+        transformation: {
+            height: { type: Number, default: Video_dimention.height },
+            width: { type: Number, default: Video_dimention.width },
+            quality: { type: Number, min: 1, max: 1000 }
+        }
+    },
+    {
+        timestamps : true
+    }
+)
+
+// model for schema 
+const Video = models?.Video || model<IVideo>('Video', videoschema);
+
+export default Video;
---
+++ types.d.ts
@@ -0,0 +1,12 @@
+import { Connection } from "mongoose";
+
+// globle veriable declare for DB connection beacuse Next.js is serverless;
+declare global {
+    var mongoose : {
+        connect : Connection | null;
+        promise : Promise<Connection> | null
+    }
+}
+
+export {};
+
