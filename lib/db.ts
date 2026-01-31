d3c5b53b0d8a61509ddc22049c0a8e29ab0f1f08
Sohaib Arshid <sohaibali1277@gmail.com>
2026-01-31 21:00:36 +0500

    Add database connection and globle veriable

---
+++ lib/db.ts
@@ -0,0 +1,35 @@
+import mongoose from "mongoose";
+const MONGODB_URL = process.env.MONGODB_URL!
+
+if (!MONGODB_URL) {
+    throw new Error("please define MONGODB_URL in env veriable")
+}
+
+let cached = global.mongoose;
+
+if (!cached) {
+    cached = global.mongoose = { connect: null, promise: null }
+}
+
+async function connectdb() {
+    if(cached.connect){
+        return cached.connect;
+    }
+
+    if(!cached.promise){
+        const opts = {
+            bufferCommands : true,
+            mazPoolSize : 10 
+        }
+        mongoose.connect(MONGODB_URL, opts)
+        .then(()=> mongoose.connection)
+    }
+
+    try{
+        cached.connect = await cached.promise
+    }catch(error){
+        cached.promise = null;
+    }
+
+    return cached.connect;
+}
