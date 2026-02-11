bfc83d6f4bdbc93b140681f71cd73a66b39ed998
Sohaib Arshid <sohaibali1277@gmail.com>
2026-02-11 08:55:16 +0500

    implementation of middleware

--- middleware.tsx
+++ middleware.tsx
@@ -1,14 +1,24 @@
 import { withAuth } from "next-auth/middleware"
 import { NextResponse } from "next/server"
 
-export default withAuth (
+export default withAuth(
     function middleware() {
         return NextResponse.next();
     },
     {
         callbacks: {
             authorized({ req, token }) {
-                if (token) return true // If there is a token, the user is authenticated
+                const { pathname } = req.nextUrl
+                if (pathname.startsWith("/api/auth") ||
+                    pathname === "/login"||
+                    pathname === "/register")
+                return true
+
+                if(pathname === "/" || pathname.startsWith("/api/video")){
+                    return true
+                }
+
+                return !!token;
             }
         },
     },
